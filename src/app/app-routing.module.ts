5cebe92243f102eeabeb2246301a0d354ef35897
JIVitian <jivitian@gmail.com>
2022-09-11 18:55:07 -0300

    Auth guard created and working!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: () => import('./modules').then(m => m.LoginModule) },
-  { path: 'home', loadChildren: () => import('./modules').then(m => m.HomeModule) },
+  {
+    path: 'login',
+    loadChildren: () => import('./modules').then(m => m.LoginModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./modules').then(m => m.HomeModule),
+    canActivate: [AuthGuard],
+  },
+  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
