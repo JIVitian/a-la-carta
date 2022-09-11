b75bd490a63144562755af508e57868595d37b3b
JIVitian <jivitian@gmail.com>
2022-09-11 16:36:04 -0300

    Login form is validating

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  email: FormControl = new FormControl<string>('', [
+    Validators.required,
+    Validators.pattern(/\S/),
+    Validators.email,
+  ]);
 
-  constructor() { }
+  password: FormControl = new FormControl<string>('', [
+    Validators.required,
+    Validators.pattern(/\S/),
+  ]);
 
-  ngOnInit(): void {
+  loginForm: FormGroup = new FormGroup({
+    email: this.email,
+    password: this.password,
+  });
+
+  constructor() {}
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      console.log('invalid form');
+    }
   }
-
 }
